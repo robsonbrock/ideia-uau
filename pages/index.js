c7f41968e6b355a2edad8d5845e10d685a9b8be7
Robson Brock <robsonbrock@gmail.com>
2021-02-10 22:48:21 -0300

    teste de navegação

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function Home() {
         <div>
             <h1>Página inicial</h1>
             <Link href="/sobre">
-                <a>Acessar página Sobre</a>
+                <a>Acessar página <b>Sobre</b></a>
             </Link>
         </div>
     )
--- pages/sobre.js
+++ pages/sobre.js
@@ -5,7 +5,7 @@ function Sobre() {
         <div>
             <h1>Sobre</h1>
             <Link href="/">
-                <a>Acessar página inicial</a>
+                <a>Acessar página <b>Home</b></a>
             </Link>
         </div>
     )

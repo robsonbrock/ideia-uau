1222245b909821331ba5cade7b268286b1c4fc76
Robson Brock <robsonbrock@gmail.com>
2021-02-10 22:42:43 -0300

    teste de navegação

---
+++ pages/sobre.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+function Sobre() {
+    return (
+        <div>Sobre</div>
+
+        <Link href="/">
+            <a>Acessar página inicial</a>
+        </Link>
+    )
+}
+export default Sobre

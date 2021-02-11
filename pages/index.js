d3bce12c7da4dd4faaa5f07f10a35aa000523a4c
Robson Brock <robsonbrock@gmail.com>
2021-02-10 22:35:57 -0300

    teste de navegação

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
+import Link from 'next/link';
+
 function Home() {
-    return <div>Home 2</div>
+    return <div>Página inicial</div>
+
+    <Link>
+        <a>Acessar página <b>Sobre</b></a>
+    </Link>
 }
 export default Home
